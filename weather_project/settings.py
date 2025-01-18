16903d655bf91cf1fc6396e85f05243d51db4a18
Maks Muradian <avtobots1235@gmail.com>
2025-01-18 20:48:17 +0200

    patch_v2

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -103,6 +103,9 @@ DATABASES = {
     )
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://hillepythonfinall-production.up.railway.app',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
